0fb16dcbfb0ce50b9092ddd56f016638e0248013
yein <inwon9892@naver.com>
2024-07-05 16:28:14 +0900

    Chapter25 paddingFrom 수정

--- Chapter25/app/src/main/java/kr/co/lion/chapter25/MainActivity.kt
+++ Chapter25/app/src/main/java/kr/co/lion/chapter25/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.paddingFromBaseline
@@ -60,8 +61,9 @@ fun MainScreen() {
         Text(
             text = "Small Text",
             modifier = Modifier.paddingFrom(
-                aligmentLine = FirstBaseline,
-                before = 80.dp, after = 0.dp
+                alignmentLine = FirstBaseline,
+                before = 80.dp,
+                after = 0.dp
             ),
             fontSize = 32.sp,
             fontWeight = FontWeight.Bold
