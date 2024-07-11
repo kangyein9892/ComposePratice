42d4d80ecc0fdf8338e2a638a2b73b7f11885d77
yein <inwon9892@naver.com>
2024-07-11 18:12:40 +0900

    Chapter30 ConstraintLayout 기본제약

---
+++ Chapter30/app/src/main/java/kr/co/lion/chapter30/MainActivity.kt
@@ -0,0 +1,64 @@
+package kr.co.lion.chapter30
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import kr.co.lion.chapter30.ui.theme.Chapter30Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Chapter30Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    ConstraintLayout(Modifier.size(width = 200.dp, height = 200.dp)) {
+        val (button1, button2, button3) = createRefs()
+
+        MyButton(text = "Button1", Modifier.constrainAs(button1) {
+            top.linkTo(parent.top, margin = 60.dp)
+            start.linkTo(parent.start, margin = 30.dp)
+        })
+    }
+}
+
+@Composable
+fun MyButton(text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = { },
+        modifier = modifier
+    ) {
+        Text(text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Chapter30Theme {
+
+    }
+}
