408232fc777732dbe2cb0a87d208c927d7d38a4a
yein <inwon9892@naver.com>
2024-07-11 19:45:19 +0900

    Chapter30 제약 집합을 이용해 제약 연결 끊기

--- Chapter30/app/src/main/java/kr/co/lion/chapter30/MainActivity.kt
+++ Chapter30/app/src/main/java/kr/co/lion/chapter30/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import kr.co.lion.chapter30.ui.theme.Chapter30Theme
 
@@ -38,40 +41,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    ConstraintLayout(Modifier.size(width = 350.dp, height = 220.dp)) {
-        val (button1, button2, button3) = createRefs()
+    ConstraintLayout(Modifier.size(width = 200.dp, height = 200.dp)) {
+        val constraints = myConstraintSet(margin = 8.dp)
 
-        // 수직 배리어, button1과 button2 끝 가장자리에서 30dp 거리에 위치
-        // 만약 폭이 다를 경우 두 컴포넌트 중 폭이 넓은 컴포넌트 끝 가장자리에서 30dp 위치
-        val barrier = createEndBarrier(button1, button2)
+        MyButton(text = "Button1", Modifier.width(200.dp).layoutId("button1"))
+    }
+}
 
-        MyButton(text = "Button1", Modifier.width(100.dp).constrainAs(button1) {
-            top.linkTo(parent.top, margin = 30.dp)
-            start.linkTo(parent.start, margin = 8.dp)
-        })
+private fun myConstraintSet(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button1 = createRefFor("button1")
 
-        MyButton(text = "Button2", Modifier.width(150.dp).constrainAs(button2) {
-            top.linkTo(button1.bottom, margin = 20.dp)
-            start.linkTo(parent.start, margin = 8.dp)
-        })
-
-        MyButton(text = "Button3", Modifier.width(100.dp).constrainAs(button3) {
-            linkTo(parent.top, parent.bottom,
-                topMargin = 8.dp, bottomMargin = 8.dp)
-            linkTo(button1.end, parent.end,
-                startMargin = 30.dp, endMargin = 8.dp)
-
-            // button3 컴포넌트 크기는 제약이 허용하는 최대공간 채우도록 설정
-            // 버튼은 높이 채우고 동시에 button1, button2 크기를 변경함에 따라 폭 조정
-            // 이를 위해서는 button3 폭과 높이 제약은 fillConstraints 로 변경
+        constrain(button1) {
+            linkTo(parent.top, parent.bottom, topMargin = margin,
+                bottomMargin = margin)
+            linkTo(parent.start, parent.end, startMargin = margin,
+                endMargin = margin)
             width = Dimension.fillToConstraints
             height = Dimension.fillToConstraints
-
-            // 처음 건 button3이 button1의 변화에 의한 제약은 받지만, button2의 변화에 의한 제약은 받지 않음
-            // -> button1, button2 끝 가장자리에 배리어 만들고, button1 끝 가장자리 기준으로 button3의 시작 모서리에 제약 적용하는 대신, 배리어를 기준으로 시작 모서리에 제약 적용
-            // 두 버튼의 폭이 늘어나면 button3을 제약하는 캐리어가 이동하므로 결과적으로 button3의 폭이 줄어든다
-            start.linkTo(barrier, margin = 30.dp)
-        })
+        }
     }
 }
 
