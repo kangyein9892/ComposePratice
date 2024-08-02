6e8bf94792a13321be7d5e3b8b6e1c674b01b0a4
강예인 <yein@gang-yein-ui-MacBookAir.local>
2024-08-02 11:34:07 +0900

    Chapter40 livedata를 이용한 viewmodel 구현

--- Chapter40/app/src/main/java/com/example/chapter40/MainActivity.kt
+++ Chapter40/app/src/main/java/com/example/chapter40/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TopLevel(model: MyViewModel = MyViewModel()) {
+    val customName: String by model.customerName.observeAsState("")
     MainScreen(count = model.customerCount) { model.increaseCount() }
 }
 
--- Chapter40/app/src/main/java/com/example/chapter40/MyViewModel.kt
+++ Chapter40/app/src/main/java/com/example/chapter40/MyViewModel.kt
@@ -3,10 +3,16 @@ package com.example.chapter40
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel: ViewModel() {
     var customerCount by mutableStateOf(0)
+    var customerName: MutableLiveData<String> = MutableLiveData("")
+
+    fun setName(name: String){
+        customerName.value = name
+    }
 
     fun increaseCount() {
         customerCount++
